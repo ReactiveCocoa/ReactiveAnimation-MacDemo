7ccc29832832c6e03465742d2e242d99d96fa0d6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-03-22 22:04:32 -0700

    Set up project and add dependencies

---
+++ MacDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacDemo
+//
+//  Created by Justin Spahr-Summers on 2015-03-22.
+//  Copyright (c) 2015 ReactiveCocoa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
